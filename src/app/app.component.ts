caacd948df65d60f0da0626e29a1c261437d9ad6
Shrikant Bhatt <shrikantbhatt92@gmail.com>
2019-04-21 12:42:19 +0530

    initial component add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'guide';
+  feactureType:string = 'recipe';
+  
+  onNavigate(e){
+    this.feactureType = e;
+  }
 }
