91c0953908a6dbdbc1450114878be84999e4b037
Shrikant Bhatt <shrikantbhatt92@gmail.com>
2019-08-12 23:12:25 +0530

    Removed onsect method

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Output ,EventEmitter} from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +6,7 @@ import { Component, OnInit, Output ,EventEmitter} from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  @Output() feactureSelected = new EventEmitter<string>()
-  constructor() { }
-
-  ngOnInit() {
-  }
-  onSelect(feacture:string){
-    this.feactureSelected.emit(feacture);
-      
-  }
-
+  ngOnInit() {}
 }
