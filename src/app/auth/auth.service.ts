820e71fd6f25605a60856576d41edfa79778a870
Shrikant Bhatt <shrikantbhatt92@gmail.com>
2019-09-13 23:56:43 +0530

    error handling method added DRY Rule

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -29,28 +29,8 @@ export class AuthService {
             )
             .pipe(
                 catchError(errorRes => {
-                    let errorMsg = 'An Unknown Error Occurred';
-                    if (!errorRes.error && !errorRes.error.error) {
-                        return throwError(errorMsg);
-                    }
-                    switch (errorRes.error.error.message) {
-                        case 'EMAIL_EXISTS':
-                            errorMsg =
-                                'The email address is already in use by another account.';
-                            break;
-                        case 'OPERATION_NOT_ALLOWED:':
-                            errorMsg = 'Password sign-in is disabled for this project.';
-                            break;
-                        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-                            errorMsg =
-                                'We have blocked all requests from this device due to unusual activity. Try again later.';
-                            break;
-
-                        default:
-                            errorMsg = 'Error Occurred!!!';
-                            break;
-                    }
-                    return throwError(errorMsg);
+                    console.error(errorRes);
+                    return this.handelError(errorRes);
                 })
             );
     }
@@ -67,30 +47,46 @@ export class AuthService {
             .pipe(
                 catchError(errorRes => {
                     console.error(errorRes);
-                    let errorMsg = 'An Unknown Error Occurred';
-                    if (!errorRes.error && !errorRes.error.error) {
-                        return throwError(errorMsg);
-                    }
-                    switch (errorRes.error.error.message) {
-                        case 'EMAIL_NOT_FOUND':
-                            errorMsg =
-                                'There is no user record corresponding to this identifier. The user may have been deleted.';
-                            break;
-                        case 'INVALID_PASSWORD':
-                            errorMsg =
-                                'The password is invalid or the user does not have a password.';
-                            break;
-                        case 'USER_DISABLED':
-                            errorMsg =
-                                'The user account has been disabled by an administrator.';
-                            break;
+                    return this.handelError(errorRes);
 
-                        default:
-                            errorMsg = 'Error Occurred!!!';
-                            break;
-                    }
-                    return throwError(errorMsg);
                 })
             );
+    }
+
+    private handelError(errorRes) {
+        let errorMsg = 'An Unknown Error Occurred';
+        if (!errorRes.error && !errorRes.error.error) {
+            return throwError(errorMsg);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_NOT_FOUND':
+                errorMsg =
+                    'There is no user record corresponding to this identifier. The user may have been deleted.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMsg =
+                    'The password is invalid or the user does not have a password.';
+                break;
+            case 'USER_DISABLED':
+                errorMsg =
+                    'The user account has been disabled by an administrator.';
+                break;
+            case 'EMAIL_EXISTS':
+                errorMsg =
+                    'The email address is already in use by another account.';
+                break;
+            case 'OPERATION_NOT_ALLOWED:':
+                errorMsg = 'Password sign-in is disabled for this project.';
+                break;
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                errorMsg =
+                    'We have blocked all requests from this device due to unusual activity. Try again later.';
+                break;
+
+            default:
+                errorMsg = 'Error Occurred!!!';
+                break;
+        }
+        return throwError(errorMsg);
     }
 }
